25d2d3687cf9c34d315a3d86312ca83f501013b5
Caio <caioborchardt19122008@gmail.com>
2024-09-30 21:23:05 -0300

    banco corrigido

--- BackEnd/BancoDeDados.sql
+++ BackEnd/BancoDeDados.sql
@@ -22,36 +22,52 @@ USE `sistemabd`;
 -- Copiando estrutura para tabela sistemabd.alunos
 CREATE TABLE IF NOT EXISTS `alunos` (
   `id_aluno` int(11) NOT NULL AUTO_INCREMENT,
-  `nome_aluno` varchar(100) NOT NULL,
-  `cpf` varchar(50) NOT NULL DEFAULT '0',
-  `data_nasc` date NOT NULL,
+  `nome_aluno` text NOT NULL,
+  `data_nasc` date DEFAULT NULL,
   `email` text DEFAULT NULL,
-  `telefone` varchar(50) DEFAULT NULL,
-  `endereco` text NOT NULL,
-  PRIMARY KEY (`id_aluno`),
-  UNIQUE KEY `cpf` (`cpf`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `senha` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id_aluno`)
+) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela sistemabd.alunos: ~0 rows (aproximadamente)
-INSERT INTO `alunos` (`id_aluno`, `nome_aluno`, `cpf`, `data_nasc`, `email`, `telefone`, `endereco`) VALUES
-	(1, 'aluno', '2147483647', '2000-12-07', 'batata@gmail.com', '47991036736', 'rua x');
+-- Copiando dados para a tabela sistemabd.alunos: ~5 rows (aproximadamente)
+DELETE FROM `alunos`;
+INSERT INTO `alunos` (`id_aluno`, `nome_aluno`, `data_nasc`, `email`, `senha`) VALUES
+	(1, 'aluno', '2000-12-07', 'batata@gmail.com', '0'),
+	(9, 'teste', '2011-01-12', 'teste', '0'),
+	(68, 'batata', '2001-02-01', 'dsfssdfg', '0'),
+	(69, 'ghdgfh', '0000-00-00', 'ghdgfh', 'ghdgfh'),
+	(70, 'batata', '2000-12-12', 'baba', '123');
 
 -- Copiando estrutura para tabela sistemabd.professores
 CREATE TABLE IF NOT EXISTS `professores` (
   `id_professor` int(11) NOT NULL AUTO_INCREMENT,
   `nome_professor` varchar(100) NOT NULL,
-  `cpf` varchar(50) NOT NULL DEFAULT '',
-  `data_nasc` date NOT NULL,
-  `email` text DEFAULT NULL,
-  `telefone` varchar(50) DEFAULT NULL,
-  `endereco` text DEFAULT NULL,
-  PRIMARY KEY (`id_professor`) USING BTREE,
-  UNIQUE KEY `cpf` (`cpf`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `data_nasc` date DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `senha` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_professor`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=111 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela sistemabd.professores: ~0 rows (aproximadamente)
-INSERT INTO `professores` (`id_professor`, `nome_professor`, `cpf`, `data_nasc`, `email`, `telefone`, `endereco`) VALUES
-	(1, 'professor', '2147483647', '2000-12-12', 'batata2@gmail.com', '12345678910', 'rua batata');
+-- Copiando dados para a tabela sistemabd.professores: ~17 rows (aproximadamente)
+DELETE FROM `professores`;
+INSERT INTO `professores` (`id_professor`, `nome_professor`, `data_nasc`, `email`, `senha`) VALUES
+	(1, 'professor', '2000-12-12', 'batata2@gmail.com', '0'),
+	(16, 'a', '1970-01-01', 'batata@gmail.com', '123'),
+	(20, '1', '1970-01-01', '1', '1'),
+	(21, 'a', '1970-01-01', 'a', '0'),
+	(30, 'as', '1970-01-01', 'a', '0'),
+	(35, 'creusa', '2000-11-10', 'sdyhfa', '0'),
+	(37, 'creudete', '2222-12-12', 'sdyhfa', '0'),
+	(38, 'teste', '2000-12-12', 'teste', '0'),
+	(44, '', '0000-00-00', '', '0'),
+	(79, 'batata', '2000-12-12', 'batata', '0'),
+	(87, 'batata', '2000-12-12', 'baba', '123'),
+	(89, 'batata', '2000-12-12', 'baba', '1234'),
+	(93, 'a', '2000-12-12', 'ad', '0'),
+	(107, 'aluno', '2000-12-12', 'aluno', '123'),
+	(108, 'batata', '2000-12-12', 'baba', '123'),
+	(109, 'a', '2000-12-12', 'a', '123'),
+	(110, 'batata', '2000-12-12', 'baba', '123');
 
 -- Copiando estrutura para tabela sistemabd.turmas
 CREATE TABLE IF NOT EXISTS `turmas` (
@@ -61,7 +77,8 @@ CREATE TABLE IF NOT EXISTS `turmas` (
   PRIMARY KEY (`id_turma`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela sistemabd.turmas: ~2 rows (aproximadamente)
+-- Copiando dados para a tabela sistemabd.turmas: ~3 rows (aproximadamente)
+DELETE FROM `turmas`;
 INSERT INTO `turmas` (`id_turma`, `materias`, `provas`) VALUES
 	(6, 'matemática', 'legal'),
 	(7, 'matemática', 'legal'),
